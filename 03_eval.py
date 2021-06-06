31d8023e5a798b5c1984e8f8085add54fe2f6467
Ana <amartina@ime.usp.br>
2021-06-06 10:10:36 -0300

    remove hardcoded counter; use an iterator instead

--- 03_eval.py
+++ 03_eval.py
@@ -5,13 +5,14 @@ from time import time
 import yaml
 
 import _base as b
-import gen_plots as p
+import geN_PLOTS as p
 from utils import glob_re
+
+N_PLOTS = 8
 
 def evaluate(base_dir, timestamp, backbone, n_channels, pretraining_dataset, finetune, split):
     model_name = f'{timestamp}_{backbone}_{n_channels}_{pretraining_dataset}_clf_ft{int(finetune)}'
     weights_file = os.path.join(base_dir, 'trained_models', model_name+'.h5')
-
     print(weights_file)
 
     trainer = b.Trainer(
@@ -22,18 +23,14 @@ def evaluate(base_dir, timestamp, backbone, n_channels, pretraining_dataset, fin
         model_name=model_name,
         weights=weights_file
     )
-
     trainer.load_weights(weights_file, skip_mismatch=False)
-
     X_test, y_test = trainer.load_data(dataset='clf', split=split)
 
     start = time()
-
-    # update model_name in order to pad n_channels
-    model_name = f'{timestamp}_{backbone}_{str(n_channels).zfill(2)}_{pretraining_dataset}_clf_ft{int(finetune)}'
     y_hat, X_features = trainer.extract_features_and_predict(X_test)
-    np.save(os.path.join(base_dir, 'npy', f'yhat_{split}_{model_name}'), y_hat)
-    np.save(os.path.join(base_dir, 'npy', f'Xf_{split}_{model_name}'), X_features)
+    model_name_ = f'yhat_{split}_{timestamp}_{backbone}_{str(n_channels).zfill(2)}_{pretraining_dataset}_clf_ft{int(finetune)}'
+    np.save(os.path.join(base_dir, 'npy', f'yhat_{split}_{model_name_}.npy'), y_hat)
+    np.save(os.path.join(base_dir, 'npy', f'yhat_{split}_{model_name_}.npy'), X_features)
     trainer.evaluate(X_test, y_test)
     print('--- minutes taken:', int((time() - start) / 60))
 
@@ -51,7 +48,7 @@ def get_finetuning_suffix(filename):
         return 'w/ finetuning'
     return 'w/o finetuning'
 
-def get_nr_channels(filename):
+def get_channels_label(filename):
     return ''.join(filter(str.isdigit, filename.split('/')[-1].split('_ft')[0]))[-2:].strip('0') + ' ch'
 
 if __name__ == '__main__':
@@ -72,7 +69,6 @@ if __name__ == '__main__':
     pretraining_dataset_lst = config['pretraining_datasets']
     finetune_lst = config['finetune']
     split = config['eval_split']
-
     umap__n_neighbors = config['umap']['n_neighbors']
 
     if not skip_predictions:
@@ -88,17 +84,17 @@ if __name__ == '__main__':
     backbone = backbone_lst[0]
 
     p.set_plt_style()
-    n_plots = 8
+    cnt_iterator = iter(range(1, N_PLOTS+1))
 
-    print(f'1/{n_plots} plotting pretraining loss curves')
+    print(f'{next(cnt_iterator)}/{N_PLOTS} plotting pretraining loss curves')
     file_list = glob_re(os.path.join(base_dir, 'mnt/history'), f'{timestamp}_{backbone}_(12|05|03)_unlabeled.json')
     p.metric_curve(
         file_list=file_list,
-        plt_labels=[get_nr_channels(f) for f in file_list],
+        plt_labels=[get_channels_label(f) for f in file_list],
         output_file='figures/exp_pretraining.pdf',
         metric='val_loss')
 
-    print(f'2/{n_plots} plotting RGB vs imagenet clf')
+    print(f'{next(cnt_iterator)}/{N_PLOTS} plotting RGB vs imagenet clf')
     file_list = glob_re(os.path.join(base_dir, 'mnt/history'), f'{timestamp}_{backbone}_03_(unlabeled|imagenet)_clf_ft(1|0).json')
     p.metric_curve(
         file_list=file_list,
@@ -108,7 +104,7 @@ if __name__ == '__main__':
         paired=True,
         color_pos=2)
 
-    print(f'3/{n_plots} plotting RGB vs imagenet clf lowdata')
+    print(f'{next(cnt_iterator)}/{N_PLOTS} plotting RGB vs imagenet clf lowdata')
     file_list = glob_re(os.path.join(base_dir, 'mnt/history'), f'{timestamp}_{backbone}_03_(unlabeled|imagenet)_clf_ft(1|0)_lowdata.json')
     p.lowdata_curve(
         file_list=file_list,
@@ -118,48 +114,48 @@ if __name__ == '__main__':
         paired=True,
         color_pos=2)
 
-    print(f'4/{n_plots} plotting channels clf')
+    print(f'{next(cnt_iterator)}/{N_PLOTS} plotting channels clf')
     file_list = glob_re(os.path.join(base_dir, 'mnt/history'), f'{timestamp}_{backbone}_(12|05|03)_unlabeled_clf_ft(1|0).json')
     p.metric_curve(
         file_list=file_list,
-        plt_labels=[get_nr_channels(f) + get_finetuning_suffix(f) for f in file_list],
+        plt_labels=[get_channels_label(f) + get_finetuning_suffix(f) for f in file_list],
         output_file='figures/exp_clf_channels.pdf',
         metric='val_accuracy',
         paired=True)
 
-    print(f'5/{n_plots} plotting channels clf lowdata')
+    print(f'{next(cnt_iterator)}/{N_PLOTS} plotting channels clf lowdata')
     file_list = glob_re(os.path.join(base_dir, 'mnt/history'), f'{timestamp}_{backbone}_(12|05|03)_unlabeled_clf_ft(1|0)_lowdata.json')
     p.lowdata_curve(
         file_list=file_list,
-        plt_labels=[get_nr_channels(f) + ' ch ' + get_finetuning_suffix(f) for f in file_list],
+        plt_labels=[get_channels_label(f) + ' ch ' + get_finetuning_suffix(f) for f in file_list],
         output_file='figures/exp_clf_channels_lowdata.pdf',
         metric='val_accuracy',
         paired=True)
 
-    print(f'6/{n_plots} plotting channels clf accuracy vs r-magnitude')
+    print(f'{next(cnt_iterator)}/{N_PLOTS} plotting channels clf accuracy vs r-magnitude')
     file_list = glob_re(os.path.join(base_dir, 'npy'), f'yhat_{split}_{timestamp}_{backbone}_(12|05|03)_unlabeled_clf_ft1.npy')
     p.acc_attribute_curve(
         file_list=file_list,
-        plt_labels=[get_nr_channels(f) for f in file_list],
+        plt_labels=[get_channels_label(f) for f in file_list],
         output_file=f'figures/exp_clf_channels_acc-r_{split}.pdf',
         dataset_file='datasets/clf.csv',
         split=split,
         attribute='r',
         legend_location='lower left')
 
-    print(f'7/{n_plots} plotting umap projections')
-    file_list = glob_re(os.path.join(base_dir, 'npy'), f'Xf_{split}_{timestamp}_{backbone}_(12|05|03)_(imagenet|unlabeled)_clf_ft1.npy')
+    print(f'{next(cnt_iterator)}/{N_PLOTS} plotting umap projections')
+    file_list = glob_re(os.path.join(base_dir, 'npy'), f'Xf_{split}_{timestamp}_{backbone}_(12|05|03)_(imagenet|unlabeled)_clf_ft0.npy')
     p.umap_scatter(
         file_list=file_list,
-        plt_labels=[get_dataset_label(f) + '; ' + get_nr_channels(f) for f in file_list],
+        plt_labels=[get_dataset_label(f) + '; ' + get_channels_label(f) for f in file_list],
         output_file=f'figures/exp_umap_{split}.pdf',
         n_neighbors=umap__n_neighbors)
 
-    print(f'8/{n_plots} plotting umap projections colored by class')
-    file_list = glob_re(os.path.join(base_dir, 'npy'), f'Xf_{split}_{timestamp}_{backbone}_(12|05|03)_(imagenet|unlabeled)_clf_ft1.npy')
+    print(f'{next(cnt_iterator)}/{N_PLOTS} plotting umap projections colored by class')
+    file_list = glob_re(os.path.join(base_dir, 'npy'), f'Xf_{split}_{timestamp}_{backbone}_(12|05|03)_(imagenet|unlabeled)_clf_ft0.npy')
     p.umap_scatter(
         file_list=file_list,
-        plt_labels=[get_dataset_label(f) + '; ' + get_nr_channels(f) for f in file_list],
+        plt_labels=[get_dataset_label(f) + '; ' + get_channels_label(f) for f in file_list],
         output_file=f'figures/exp_umap_{split}_classes_neighbours{umap__n_neighbors}.pdf',
         dataset_file='datasets/clf.csv',
         split=split,
