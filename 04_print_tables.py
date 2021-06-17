fa6224db1074e72f4e214f1ad477faa48a7587b1
Ana <amartina@ime.usp.br>
2021-06-17 20:18:14 -0300

    add more tables to postprocessing scripts

--- 04_print_tables.py
+++ 04_print_tables.py
@@ -1,5 +1,7 @@
+import os
+
 from label_the_sky.config import config
-from label_the_sky.postprocessing.table import agg_histories, print_latex
+from label_the_sky.postprocessing.table import agg_histories, print_history_latex, print_yhat
 from label_the_sky.training.trainer import MAG_MAX
 from label_the_sky.utils import glob_re
 
@@ -8,25 +10,46 @@ timestamp = config['timestamp']
 
 cnt_iterator = iter(range(100))
 
-print(f'\n{str(next(cnt_iterator)).zfill(2)} pretraining errors')
+print(f'\n{str(next(cnt_iterator)).zfill(2)} pretraining validation errors\n')
 history_lst = agg_histories(
     file_list=glob_re('../history', f'{timestamp}_{backbone}_(12|05|03)_unlabeled.json'),
     metric='val_loss',
     mode='min',
     rescale_factor=MAG_MAX)
-print_latex(history_lst, cols=['n_channels', 'val_loss'])
+print_history_latex(history_lst, cols=['n_channels', 'val_loss'])
 
-print(f'\n{str(next(cnt_iterator)).zfill(2)} channels clf')
+print(f'\n{str(next(cnt_iterator)).zfill(2)} pretraining test errors per channel (12 channel model)\n')
+print_yhat(
+    npy_file=os.path.join(os.environ['HOME'], 'npy', 'yhat_unlabeled-test_0206_vgg_12_unlabeled.npy'),
+    dataset_file='datasets/unlabeled.csv',
+    split='test')
+
+print(f'\n{str(next(cnt_iterator)).zfill(2)} RGB clf\n')
+history_lst = agg_histories(
+    file_list=glob_re('../history', f'{timestamp}_{backbone}_03_(imagenet|unlabeled)_clf_ft(0|1).json'),
+    metric='val_accuracy',
+    mode='max')
+print_history_latex(
+    history_lst,
+    cols=['weights', 'finetune', 'val_accuracy'],
+    groupby_cols=['weights', 'finetune'])
+
+print(f'\n{str(next(cnt_iterator)).zfill(2)} channels clf\n')
 history_lst = agg_histories(
     file_list=glob_re('../history', f'{timestamp}_{backbone}_(12|05|03)_unlabeled_clf_ft(0|1).json'),
     metric='val_accuracy',
     mode='max')
-print_latex(
+print_history_latex(
     history_lst,
     cols=['n_channels', 'finetune', 'val_accuracy'],
     groupby_cols=['n_channels', 'finetune'])
 
-# df.drop(columns=['timestamp', 'backbone', 'runs'], inplace=True)
-# dfg = df.groupby(['weights', 'finetune', 'n_channels']).first().unstack().unstack().T
-# print(dfg)
-# print(dfg.to_latex())
+print(f'\n{str(next(cnt_iterator)).zfill(2)} channels clf lowdata\n')
+history_lst = agg_histories(
+    file_list=glob_re('../history', f'{timestamp}_{backbone}_(12|05|03)_unlabeled_clf_ft(0|1)_lowdata.json'),
+    metric='val_accuracy',
+    mode='max')
+print_history_latex(
+    history_lst,
+    cols=['n_channels', 'finetune', 'val_accuracy'],
+    groupby_cols=['n_channels', 'finetune'])
--- label_the_sky/postprocessing/table.py
+++ label_the_sky/postprocessing/table.py
@@ -2,7 +2,9 @@ import json
 import numpy as np
 import pandas as pd
 
+from label_the_sky.config import BANDS
 from label_the_sky.training.trainer import MAG_MAX
+from label_the_sky.utils import get_channels_label
 
 
 AGG_FN = {
@@ -29,7 +31,10 @@ def agg_histories(file_list, metric='val_loss', mode='min', agg_mode='mean', res
             history = json.load(json_file)
             if type(history) == dict:
                 history = [history]
-            lst = [run_fn(history_run[metric])*rescale_factor for history_run in history]
+            if type(history[0]) == list:
+                lst = [run_fn(history_run[-1][metric])*rescale_factor for history_run in history]
+            else:
+                lst = [run_fn(history_run[metric])*rescale_factor for history_run in history]
             history_data = {
                 f'{metric}': agg_fn(lst),
                 'runs': len(lst)
@@ -44,12 +49,36 @@ def agg_histories(file_list, metric='val_loss', mode='min', agg_mode='mean', res
             history_lst.append(history_data)
     return history_lst
 
-def print_latex(history_lst, cols, groupby_cols=None):
+def get_df_latex(df, index=False):
+    latex_str = df.to_latex(index=index)
+    latex_str = latex_str.replace('\\toprule\n', '')
+    latex_str = latex_str.replace('\\bottomrule\n', '')
+    latex_str = latex_str.replace('\\midrule', '\\hline')
+    return latex_str
+
+def print_history_latex(history_lst, cols, groupby_cols=None):
     df = pd.DataFrame.from_dict(history_lst)
     df = df[cols]
     if groupby_cols:
         df = df.groupby(groupby_cols).first()
         for _ in range(len(groupby_cols)-1):
             df = df.unstack()
+    else:
+        df.sort_values(by=df.columns[0], inplace=True)
     index = True if groupby_cols else False
-    print(df.to_latex(index=index))
+    print(get_df_latex(df, index=index))
+
+def print_yhat(npy_file, dataset_file, split='test'):
+    df = pd.read_csv(dataset_file)
+    df = df[df.split==split]
+    y_hat = np.load(npy_file)
+    y = df[BANDS].values
+    y_error = df[[band+'_err' for band in BANDS]].values
+    diff = np.abs(y - y_hat)
+
+    df_out = pd.DataFrame()
+    df_out['channel'] = BANDS
+    df_out['mean absolute error'] = np.mean(diff, axis=0).round(4)
+    df_out['mean uncertainty'] = np.mean(y_error, axis=0).round(4)
+
+    print(get_df_latex(df_out))
